e2051bc2f83197675b360a63e25194b0e3937608
p0rokh <anton.corolev@academmedia.com>
2016-10-30 21:00:40 +0300

    Настройка проекта

---
+++ WeatherAPI/WeatherAPI/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  WeatherAPI
+//
+//  Created by Anton Korolev on 30.10.16.
+//  Copyright © 2016 AntonKorolev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
